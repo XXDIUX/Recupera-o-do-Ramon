a46991a9223263e2bf7d1c560c899ca5b20a4401
LUCAS MEDEIROS PEREIRA <lucas_pereira24@estudante.sesisenai.org.br>
2025-04-24 08:59:06 -0300

    Criação das classes Guerreiro, Mago e Arqueiro herdando de Personagem

--- script.js
+++ script.js
@@ -1,31 +1,25 @@
 class Personagem{
-   cuctor(mana, vida, forca){
+    constructor(mana, vida, forca){
     mana = mana,
     this.forca = forca,
     this.vida = vida
    }
+
 }
 class Guerreiro extends Personagem{
     constructor(mana, vida, forca){
         super(mana, vida, forca)
-    mana = mana,
-    forca = forca,
-    vida = vida
+    this.Atacar = Atacar;
     }
+    
 }
 class Mago extends Personagem{
     constructor(mana, vida, forca){
         super(mana, vida, forca)
-        mana = mana,
-        forca = forca,
-        vida = vida
     }
 }
  class Arqueiro extends Personagem{
     constructor(mana, vida, forca){
         super(mana, vida, forca)
-        mana = mana,
-        forca = forca,
-        vida = vida
     }
 }
