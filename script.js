c04ec681a48ffd8eb64ee64fda13febfc41e6f21
LUCAS MEDEIROS PEREIRA <lucas_pereira24@estudante.sesisenai.org.br>
2025-04-24 08:23:05 -0300

    Classes filha

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
-class Personagem {
-    #mana
-    #vida
-    
+class Personagem{
+   cuctor(mana, vida, forca){
+    mana = mana,
+    this.forca = forca,
+    this.vida = vida
+   }
+}
+class Guerreiro extends Personagem{
+    constructor(mana, vida, forca){
+        super(mana, vida, forca)
+    mana = mana,
+    forca = forca,
+    vida = vida
+    }
+}
+class Mago extends Personagem{
+    constructor(mana, vida, forca){
+        super(mana, vida, forca)
+        mana = mana,
+        forca = forca,
+        vida = vida
+    }
+}
+ class Arqueiro extends Personagem{
+    constructor(mana, vida, forca){
+        super(mana, vida, forca)
+        mana = mana,
+        forca = forca,
+        vida = vida
+    }
 }
